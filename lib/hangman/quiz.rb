7266d1c8fd50aa7507dadfa0ec69a64441672de2
Tobias Eilert <tobias.eilert@grandcentrix.net>
2017-06-08 20:35:40 +0200

    add more rules

--- lib/hangman/quiz.rb
+++ lib/hangman/quiz.rb
@@ -1,19 +1,31 @@
 class Quiz
+  attr_reader :guessed
+
   def initialize(searched_word)
     @solution = searched_word
+    @guessed = []
 
-    @letter_slots = Array.new(searched_word.length, "_")
+    @letter_slots = "_" * searched_word.length
   end
 
   def guess!(char)
-    found = @solution.index(char)
+    found = @solution.downcase.index(char.downcase)
+    return false unless found
 
     if found >= 0
-      @letter_slots[found] = char
+      @letter_slots[found] = @solution[found]
+    end
+
+    if guessed.include?(char)
+      false
+    else
+      guessed << char
+      true
     end
   end
 
   def represent
-    @letter_slots.join('')
+    @letter_slots
   end
 end
+
--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -9,10 +9,38 @@ RSpec.describe Quiz do
     expect(dog.represent).to eq("___")
   end
 
-  it "accepts a letter as guess" do
+  it "accepts a letter as guess and returns true" do
+    batman = Quiz.new "Batman"
+
+    expect(batman.guess!("m")).to eq(true)
+    expect(batman.represent).to eq("___m__")
+  end
+
+  it "accepts letter insensitive of case" do
+    batman = Quiz.new "Batman"
+
+    expect(batman.guess!("b")).to eq(true)
+    expect(batman.represent).to eq("B_____")
+
+    expect(batman.guess!("t")).to eq(true)
+    expect(batman.represent).to eq("B_t___")
+  end
+
+  it "adds a letter to the guessed list once guessed" do
     batman = Quiz.new "Batman"
     batman.guess! "m"
 
-    expect(batman.represent).to eq("___m__")
+    expect(batman.guessed).to eq(["m"])
+  end
+
+  it "returns false if a letter is guessed twice and doesn't add it again" do
+    batman = Quiz.new "Batman"
+    batman.guess! "m"
+
+    expect(batman.guess!("m")).to eq(false)
+    expect(batman.guessed).to eq(["m"])
+  end
+
+  it "reveals all occurrences of a letter" do
   end
 end
